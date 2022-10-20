215ba914d0dd6a6a85d380d0bc15e345640e9d04
Shoaib Akhtar Ansari <shoaib.ansari5398@gmail.com>
2022-10-19 22:41:33 -0700

    taking input and console

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
 
 function clickEventHandler(){
     console.log("clicked");
+    console.log("Input:",txtInput.value);
 }
 
 btnTranslate.addEventListener("click",clickEventHandler);
