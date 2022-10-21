d247ee7248d12c52f6462eb8c68650ee788ceca0
Shoaib Akhtar Ansari <shoaib.ansari5398@gmail.com>
2022-10-20 22:29:01 -0700

    functionality complete

--- app.js
+++ app.js
@@ -2,8 +2,26 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-function clickEventHandler(){
-    outputDiv.innerHTML = txtInput.value;
+// var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationalUrl(text) {
+    return serverUrl + "?" + "text=" + text
 }
 
-btnTranslate.addEventListener("click",clickEventHandler);
+function errorHandler(error) {
+    console.log("error handler",error)
+}
+
+function clickHandler(){
+
+    var inputText = txtInput.value;
+
+    fetch(getTranslationalUrl(inputText))
+    .then(response => response.json())
+    .then(json => outputDiv.innerHTML = json.contents.translated)
+    .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click",clickHandler);
